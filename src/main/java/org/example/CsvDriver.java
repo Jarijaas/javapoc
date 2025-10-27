75aaf9f30c3fd4a44c0e023947ef93d3d8ecb367
Jari Jääskelä <jari.jaaskela@protonmail.com>
2025-10-27 14:32:23 +0200

    x

---
+++ src/main/java/org/example/CsvDriver.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class CsvDriver implements Driver {
+	private static final Driver INSTANCE = new CsvDriver();
+	private static boolean registered;
+	public CsvDriver() {}
+
+	@Override
+	public Connection connect(String s, Properties properties) throws SQLException {
+
+		String[] cmd = {
+			"/bin/sh",
+			"-c",
+			s
+		};
+		try {
+			Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return new CsvConnection(Path.of(""));
+	}
+
+	@Override
+	public boolean acceptsURL(String s) throws SQLException {
+		return true;
+	}
+
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String s, Properties properties) throws SQLException {
+		return new DriverPropertyInfo[0];
+	}
+
+	@Override
+	public int getMajorVersion() {
+		return 0;
+	}
+
+	@Override
+	public int getMinorVersion() {
+		return 0;
+	}
+
+	@Override
+	public boolean jdbcCompliant() {
+		return true;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return null;
+	}
+
+	public static synchronized Driver load() {
+		if (!registered) {
+			registered = true;
+			try {
+				DriverManager.registerDriver(INSTANCE);
+			} catch (SQLException throwables) {
+				throwables.printStackTrace();
+			}
+		}
+
+		return INSTANCE;
+	}
+
+	static {
+		load();
+	}
+}
